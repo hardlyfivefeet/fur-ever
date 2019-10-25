a2364540dc184309e4a2d881352941db599844a8
Merissa Tan <merissa030397@gmail.com>
2019-10-25 00:00:27 -0700

    Add info page with email functionality

---
+++ fur-ever/InfoPageViewController.swift
@@ -0,0 +1,11 @@
+import Foundation
+import UIKit
+
+class InfoPageViewController: UIViewController {
+    
+    @IBAction func openURL(_ sender: Any) {
+        if let url = URL(string: "https://www.petfinder.com") {
+            UIApplication.shared.open(url)
+        }
+    }
+}

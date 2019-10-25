809397d42e9e70cbf51a75ce4d84ebe50cfa4a7c
Merissa Tan <merissa030397@gmail.com>
2019-10-24 20:18:07 -0700

    Fix navigation controller problems

---
+++ fur-ever/HomePageViewController.swift
@@ -0,0 +1,26 @@
+import Foundation
+import UIKit
+
+class HomePageViewController : UIViewController {
+
+    override func viewDidLoad() {
+     super.viewDidLoad()
+    }
+ 
+    @IBAction func locationButton(_ sender: Any) {
+        (sender as! UIButton).tag = 0
+        performSegue(withIdentifier: "tabBarSegue", sender: sender)
+    }
+    @IBAction func organizationButton(_ sender: Any) {
+        (sender as! UIButton).tag = 1
+        performSegue(withIdentifier: "tabBarSegue", sender: sender)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "tabBarSegue" {
+            if let vc = segue.destination as? UITabBarController {
+                vc.selectedIndex = (sender as! UIButton).tag
+            }
+        }
+    }
+}
